d39d26d6bf213f70f1182acd48b593e3148dbed8
unknown <sepulvedabautista08@gmail.com>
2026-04-01 10:57:11 -0300

    mi primer commit

---
+++ practica1.py
@@ -0,0 +1,18 @@
+#practico python
+#fecha 27/03/26
+#1- imprimir un nombre impreso por teclado
+nombre=input("ingresa tu nombre ")
+print (nombre)
+#en python las variables son de tipado dinamico
+print(type(nombre))
+
+edad=int(input("ingresa tu edad "))
+print (edad)
+#en python las variables son de tipado dinamico
+print(type(edad))
+
+#ingresar un numero por teclado y el programa me tiene que dar el doble, el triple y la mitad
+numero=float(input("ingresa un numero "))
+print("el numero doble es: ", numero*2)
+print("el numero triple es: ", numero*3)
+print("el numero divido en dos es: ", numero/2)
