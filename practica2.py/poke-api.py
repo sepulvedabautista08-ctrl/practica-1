3eac106680c741f7594b6763e77f23ae4c0e9c40
unknown <sepulvedabautista08@gmail.com>
2026-04-10 10:39:00 -0300

    mi segundo commit

--- practica2.py/poke-api.py
+++ practica2.py/poke-api.py
@@ -1,7 +1,16 @@
 import requests
+#importa una libreria para que el cliente haga solicitudes
 url="https://pokeapi.co/api/v2/pokemon/pikachu"
+#guarda la direccion web
 respuesta=requests.get(url)
+#hace la peticion a la url
 datos=respuesta.json()
+#datos esta guardando un json
 print("nombre:", datos["name"])
+#muestra el nombre
 print("altura:", datos["height"])
+#muestra la altura
 print("peso:", datos["weight"])
+#muestra el peso
+print(respuesta)
+#muestra la respuesta
